54c563066a4d43a4fc9855dbb2eb32377ca6bc97
Michael Furmur <m.furmur@gmail.com>
2017-05-10 12:43:45 +0300

    add allowed_fields option for clickhouse statistics section

--- server/src/opts/opts_helpers.h
+++ server/src/opts/opts_helpers.h
@@ -3,6 +3,7 @@
 #define DCFG_INT(name) CFG_INT((char*)#name,0,CFGF_NODEFAULT)
 #define DCFG_BOOL(name) CFG_BOOL((char*)#name,cfg_false,CFGF_NODEFAULT)
 #define DCFG_STR(name) CFG_STR((char*)#name,NULL,CFGF_NODEFAULT)
+#define DCFG_STR_LIST(name) CFG_STR((char *)#name,NULL,CFGF_LIST)
 
 #define DCFG_SEC(name,sec_opt_name,sec_opts) CFG_SEC((char *)#name,sec_opt_name,sec_opts)
 
